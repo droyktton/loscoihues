da5cca2dc0ab2d10e5dc6509d4e023d21411b1b0
Ale K <droyktton@gmail.com>
2020-09-11 21:17:34 -0300

    arreglo demora

--- parametros.js
+++ parametros.js
@@ -1,1 +1,15 @@
 difRN=0;
+difBsAs=0;
+difCABA=0;
+difCba=0;
+difChaco=0;
+difEntreRios=0;
+difJujuy=0;
+difLaRioja=0;
+difMendoza=0;
+difNeu=0;
+difSalta=0;
+difSantaCruz=0;
+difSantaFe=0;
+difTierraDelFuego=0;
+difTucuman=0;
